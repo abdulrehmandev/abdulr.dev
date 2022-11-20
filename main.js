d6c71d2d55d23eab9ff02a01cf6bec70c1705782
Abdul Rehman <beingabdulr@gmail.com>
2022-11-20 00:25:45 -0800

    Add Other Sections

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ headerToggle.addEventListener('click', () => {
         header.getAttribute('data-md-expanded') === 'false'
     )
 })
+
+const bannerHead1 = document.querySelector('.heading-1')
+const bannerHead2 = document.querySelector('.heading-2')
+
+window.addEventListener('scroll', () => {
+    let scrollValue = window.scrollY
+
+    if (scrollValue >= 170) {
+        let value = scrollValue - 170
+        bannerHead1.style.transform = `translate(${(value / 2)}px)`
+        bannerHead2.style.transform = `translate(-${value / 2}px)`
+    }
+})
