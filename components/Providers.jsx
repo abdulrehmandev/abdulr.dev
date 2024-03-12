3b21541727a374fb3710d5357b08ba7ed6bdfa1d
Abdul R <beingabdulr@gmail.com>
2024-03-12 15:07:52 +0500

    Delete Previous files

--- components/Providers.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ThemeProvider } from "next-themes";
-
-const Providers = ({ children }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
-};
-
-export default Providers;
--- components/SkillButton.jsx
+++
@@ -1,9 +0,0 @@
-const SkillButton = ({ text }) => {
-  return (
-    <div className="cursor-default dark:bg-amber-800/20 bg-amber-300/25  px-4 py-1 rounded-full text-sm text-amber-600 dark:text-amber-400">
-      {text}
-    </div>
-  );
-};
-
-export default SkillButton;
