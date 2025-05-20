c2230439bb8971218ec30cc4c7a6c22358a910cf
Abdul R <beingabdulr@gmail.com>
2025-05-21 00:06:09 +0500

    Remove all files

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./layout";
-export * from "./ui";
-export * from "./sections";
--- src/components/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Container } from "./container";
-export { default as Footer } from "./footer";
-export { default as Section } from "./section";
