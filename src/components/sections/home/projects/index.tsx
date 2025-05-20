46d0fa2b5f99de3056180c61a0d1662aae394478
Abdul R <beingabdulr@gmail.com>
2025-05-20 23:32:46 +0500

    fix: responsiveness issues

--- src/components/sections/home/projects/index.tsx
+++ src/components/sections/home/projects/index.tsx
@@ -5,7 +5,9 @@ import { ProjectCard } from "./project-card";
 export function Projects() {
   return (
     <section>
-      <Container className="grid grid-cols-2 gap-4">{makeProjects()}</Container>
+      <Container className="grid grid-cols-1 md:grid-cols-2 gap-2 lg:gap-4">
+        {makeProjects()}
+      </Container>
     </section>
   );
 }
@@ -14,14 +16,14 @@ function makeProjects() {
   const projects = Object.values(ProjectsData);
   return (
     <>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 lg:gap-4">
         {projects
           .filter((_, i) => i % 2 === 0)
           .map((project, i) => (
             <ProjectCard key={project.title + i} project={project} />
           ))}
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 lg:gap-4">
         {projects
           .filter((_, i) => i % 2 === 1)
           .map((project, i) => (
