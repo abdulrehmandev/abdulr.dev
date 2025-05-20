c7761ba733ef1c28e91a603fa309ccdd00aebed2
Abdul R <beingabdulr@gmail.com>
2025-05-20 23:44:21 +0500

    fix: build errors

--- src/components/lenis.tsx
+++ src/components/lenis.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import React, { PropsWithChildren } from "react";
+import * as React from "react";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import type { PropsWithChildren } from "react";
 
 export function Lenis({ children }: PropsWithChildren) {
-  return (
-    <ReactLenis
-      // options={{ duration: 2 }}
-      root
-    >
-      {children}
-    </ReactLenis>
-  );
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return <ReactLenis root>{children as any}</ReactLenis>;
 }
