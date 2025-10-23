0de8149a92914d5ba0323a851c0db480e1d0102b
Abdul Rehman <beingabdulr@gmail.com>
2025-10-24 01:38:30 +0500

    feat: create a primary card component and use in case studies

--- src/app/(root)/study/page.tsx
+++ src/app/(root)/study/page.tsx
@@ -1,13 +1,12 @@
-import { Metadata } from "next";
-import Link from "next/link";
-import { RabbitIcon } from "lucide-react";
-import { getAllStudies } from "@src/lib/mdx";
 import {
   PageHeader,
   PageHeaderDescription,
   PageHeaderHeading,
 } from "@src/components/page-header";
-import { Card, CardContent, CardHeader, CardTitle } from "@src/ui/card";
+import { PrimaryCard } from "@src/components/primary-card";
+import { getAllStudies } from "@src/lib/mdx";
+import { Expand, RabbitIcon } from "lucide-react";
+import { Metadata } from "next";
 
 export const dynamic = "force-static";
 export const revalidate = false;
@@ -31,61 +30,28 @@ export default function StudyPage() {
       </PageHeader>
       <div className="grid gap-4 md:grid-cols-3">
         {caseStudies.map((study) => (
-          <Card key={study.slug} asChild>
-            <Link
-              href={`/study/${study.slug}`}
-              className="group transition-all gap-0 block"
-            >
-              <div className="relative p-2 border-b">
-                <svg className="pointer-events-none absolute inset-0 -z-1 size-full select-none text-blue-300 dark:text-blue-400/10">
-                  <defs>
-                    <pattern
-                      id=":Su:"
-                      width="4"
-                      height="4"
-                      patternUnits="userSpaceOnUse"
-                      patternTransform="rotate(45)"
-                    >
-                      <line
-                        x1="0"
-                        y1="0"
-                        x2="0"
-                        y2="4"
-                        stroke="currentColor"
-                        stroke-width="1.5"
-                      ></line>
-                    </pattern>
-                  </defs>
-                  <rect width="100%" height="100%" fill="url(#:Su:)"></rect>
-                </svg>
-                <div className="flex size-full relative items-center justify-center">
-                  <img
-                    src="https://images.ctfassets.net/kftzwdyauwt9/6bU3sUQ4xHv1r7eyHJxaWO/868bcc4d42d2c06d406ada5b20a44237/Apps_in_ChatGPT.png?w=1920&q=90&fm=webp"
-                    className="aspect-4/3 size-full border rounded-xs object-cover"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <CardHeader className="pt-4">
-                <CardTitle>{study.title}</CardTitle>
-              </CardHeader>
-              <CardContent className="flex items-center gap-2 justify-between mt-1 pb-4">
-                <div className="flex gap-3 items-center text-sm">
-                  <p className="font-medium">Product</p>
-                  <p className="text-muted-foreground">
-                    {new Date(study.date).toLocaleDateString("en-US", {
-                      year: "numeric",
-                      month: "short",
-                    })}
-                  </p>
-                </div>
-                <p className="text-xs text-right text-muted-foreground">
-                  {study.readTime}
+          <PrimaryCard
+            key={study.title}
+            image="https://images.ctfassets.net/kftzwdyauwt9/6bU3sUQ4xHv1r7eyHJxaWO/868bcc4d42d2c06d406ada5b20a44237/Apps_in_ChatGPT.png?w=1920&q=90&fm=webp"
+            title={study.title}
+            actions={[{ icon: <Expand />, label: "Expand" }]}
+            href={`/study/${study.slug}`}
+          >
+            <div className="flex item-center gap-2 w-full justify-between">
+              <div className="flex gap-3 items-center text-sm">
+                <p className="font-medium">Product</p>
+                <p className="text-muted-foreground text-xs">
+                  {new Date(study.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                  })}
                 </p>
-              </CardContent>
-              {/* <Image src="/" /> */}
-            </Link>
-          </Card>
+              </div>
+              <p className="text-xs text-right text-muted-foreground">
+                {study.readTime}
+              </p>
+            </div>
+          </PrimaryCard>
         ))}
         <div className="p-4 w-full h-full flex flex-col gap-1 items-center justify-center text-muted-foreground group/more">
           <RabbitIcon
@@ -98,43 +64,3 @@ export default function StudyPage() {
     </div>
   );
 }
-
-/**
- *  <div className="grid gap-6 md:grid-cols-3">
-   {caseStudies.map((study) => (
-     <Link
-       key={study.slug}
-       href={`/study/${study.slug}`}
-       className="group transition-all"
-     >
-       <img
-         src="https://images.ctfassets.net/kftzwdyauwt9/6bU3sUQ4xHv1r7eyHJxaWO/868bcc4d42d2c06d406ada5b20a44237/Apps_in_ChatGPT.png?w=1920&q=90&fm=webp"
-         className="aspect-square w-full h-fit rounded-lg object-cover"
-         alt=""
-       />
-       <h3 className="font-medium text-lg mt-4">{study.title}</h3>
-       <div className="flex items-center gap-2 justify-between mt-2">
-         <div className="flex gap-3 items-center text-sm">
-           <p className="font-medium">Product</p>
-           <p className="text-muted-foreground">
-             {new Date(study.date).toLocaleDateString("en-US", {
-               year: "numeric",
-               month: "short",
-             })}
-           </p>
-         </div>
-         <p className="text-xs text-right text-muted-foreground">
-           {study.readTime}
-         </p>
-       </div>
-     </Link>
-   ))}
-   <div className="p-4 w-full h-full flex flex-col gap-1 items-center justify-center text-muted-foreground group/more">
-     <RabbitIcon
-       className="size-8 group-hover/more:-scale-x-100"
-       strokeWidth={1}
-     />
-     <p className="text-xs">More coming soon!</p>
-   </div>
- </div>
- */
--- src/components/primary-card.tsx
+++ src/components/primary-card.tsx
@@ -1,20 +1,91 @@
-import React from "react";
+"use client";
+
 import { cn } from "@src/lib/utils";
+import { Card, CardContent, CardHeader, CardTitle } from "@src/ui/card";
+import NextLink from "next/link";
+import React from "react";
+
+type PrimaryCardProps = React.ComponentProps<"div"> & {
+  actions?: Array<{
+    onClick?: () => void;
+    label: string;
+    icon: React.JSX.Element;
+  }>;
+  href?: string;
+  image: string;
+  title: string;
+};
 
 export function PrimaryCard({
-  children,
   className,
+  href,
+  image,
+  title,
+  actions,
+  children,
   ...props
-}: React.ComponentProps<"div">) {
+}: PrimaryCardProps) {
+  const Comp = href ? NextLink : React.Fragment;
   return (
-    <div
-      className={cn(
-        "outline-1 outline-border/50 rounded-[18px] p-1.5 [box-shadow:0_0px_60px_-20px_#ffffff1f_inset] dark:[box-shadow:3px_3px_40px_5px_#c6c6c615_inset] bg-radiial from-background to-muted/30 from-60%",
-        className
-      )}
+    <Card
+      className={cn("group transition-all gap-0 block group/card", className)}
+      asChild={href ? true : false}
       {...props}
     >
-      <div className="border rounded-[12px] p-4">{children}</div>
-    </div>
+      <Comp href={href!}>
+        <div className="relative p-1.5 border-b">
+          <svg className="pointer-events-none absolute inset-0 size-full select-none text-blue-300 dark:text-blue-400/10">
+            <defs>
+              <pattern
+                id=":Su:"
+                width="4"
+                height="4"
+                patternUnits="userSpaceOnUse"
+                patternTransform="rotate(45)"
+              >
+                <line
+                  x1="0"
+                  y1="0"
+                  x2="0"
+                  y2="4"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                ></line>
+              </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#:Su:)"></rect>
+          </svg>
+          <div className="flex size-full relative items-center justify-center">
+            <img
+              src={image}
+              className="aspect-4/3 size-full border rounded-xs object-cover"
+              alt={title}
+            />
+          </div>
+          {actions && actions.length !== 0 && (
+            <div className="absolute z-1 inset-x-0 bottom-0 bg-card text-primary text-sm flex items-center justify-center border-t border-border-primary translate-y-[110%] group-hover/card:translate-y-0 transition duration-300 *:flex *:cursor-pointer *:[&_svg]:size-3.25 *:items-center *:gap-2 *:py-1.5 *:px-1">
+              {actions.map((act) => (
+                <button
+                  key={act.label}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    act.onClick?.();
+                  }}
+                >
+                  Expand {act.icon}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="relative z-1 bg-card">
+          <CardHeader className="pt-4">
+            <CardTitle>{title}</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-1 pb-4">{children}</CardContent>
+        </div>
+      </Comp>
+    </Card>
   );
 }
